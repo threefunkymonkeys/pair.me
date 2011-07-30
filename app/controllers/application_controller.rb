91b5d571312fcd8703b64b29a4643b274190afe9
Leonardo Mateo <leonardomateo@gmail.com>
2011-07-30 12:30:46 -0300

    Adds require user before filter to ensure logged it user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def require_user
+    redirect_to login_path unless current_user
+  end
+
   private 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
